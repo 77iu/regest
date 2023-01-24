44b62574e2e0e2ab4fcd19a33dbd5fbb0bdb1635
77iu <112532158+77iu@users.noreply.github.com>
2023-01-24 17:49:50 -0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+let section = document.querySelector('section')
+
+let boton = document.getElementById("boton");
+let meses = document.getElementById("meses");
+
+boton.onclick= function (){
+    meses.classList.toggle("mesesCh");
+   
+}
